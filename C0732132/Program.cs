bb6b61afd5f0d472d8bdea7693a1fe9400711ae8
visualstudio <visualstudio@122-PC19>
2019-01-25 20:59:30 -0500

    new villages

--- C0732132/Program.cs
+++ C0732132/Program.cs
@@ -23,20 +23,41 @@ namespace C0732132
 
     class Countryside
     {
-        
+        Village Alst;
+        Village Maeland;
+        Village schenig;
+
+
+        public void InitializeMap()
+        {
+           
+            Alst = new Village();
+            Maeland = new Village();
+            Maeland.isAstrildeHere = true;
+            schenig = new Village();
+
+            Alst.NextVillage = Maeland;
+            Maeland.NextVillage = schenig;
+            schenig.NextVillage = Alst;
+        }
+         public void WalkAround()
+        {
+         Village IntialVillage = this.Maeland;
+
+        }
+
     }
 
 
     class Village
     {
-        Village PreviousVillage;
-        Village NextVillage;
-        String VillageName;
-        bool isAstrildeHere = false;
+        public Village PreviousVillage;
+        public Village NextVillage;
+        public String VillageName;
+        public bool isAstrildeHere = false;
         public Village() { }
-
+         
     }
 
-
-
+    
 }
